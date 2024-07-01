b4da46bf3d75a40c2a71b3322a526f83cd69a026
giovanni2299 <giovanni-sorrentino@hotmail.it>
2024-07-01 15:16:30 +0200

    ADD: ApartmentController create view

--- app/Http/Controllers/Admin/ApartmentController.php
+++ app/Http/Controllers/Admin/ApartmentController.php
@@ -16,7 +16,7 @@ class ApartmentController extends Controller
     }
 
     public function create(){
-        
+        return view('admin.apartments.create');
     }
 
     public function store(){
---
+++ resources/views/admin/apartments/create.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>questa è la create</h1>
+@endsection
