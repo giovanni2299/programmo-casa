cb1aa5bf4268d5dc1c5435283ae265c3b9a7593b
RoccoCerro <rcerro02@gmail.com>
2024-07-01 13:06:56 +0200

    Add: destroy for crud services

--- app/Http/Controllers/Admin/ServiceController.php
+++ app/Http/Controllers/Admin/ServiceController.php
@@ -61,8 +61,10 @@ class ServiceController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Service $service)
     {
-        //
+        $service->delete();
+
+        return to_route('admin.services.index');
     }
 }
--- resources/views/admin/services/index.blade.php
+++ resources/views/admin/services/index.blade.php
@@ -12,20 +12,32 @@
     <div class="container p-2">
       <div class="row">
         @foreach ($services as $service)
-          <div class="col-2 p-2">
-            <div class="card p-2">
-              <div class="card-text">
-                <a href="{{ route('admin.services.show', $service) }}">
-                  {{ $service->name }}
-                </a>
-              </div>
-              <div class="card-btn">
-                <a class="btn btn-secondary text-white" href="">MODIFICA</a>
-                <a class="btn btn-danger" href="">ELIMINA</a>
-              </div>
-            </div>
+        <div class="col-2 p-2">
+        <div class="card p-2">
+          <div class="card-text">
+          <a href="{{ route('admin.services.show', $service) }}">
+            {{ $service->name }}
+          </a>
           </div>
-        @endforeach
+          <div class="card-btn">
+            <a class="btn btn-secondary text-white" href="">MODIFICA</a>
+            <form class="services-destroy-form" action="{{ route('admin.services.destroy', $service) }}" method="POST">
+
+              @csrf
+              @method('DELETE')
+
+              <button class="btn btn-danger link-danger text-white">Elimina</button>
+
+              <!-- <div class="d-none modal-delete" >
+                <h5>Sei sicuro di voler eliminare?</h5>
+                <button class="btn-yes">si</button>
+                <button class="btn-no">no</button>
+              </div> -->
+            </form>
+          </div>
+        </div>
+      </div>
+    @endforeach
       </div>
     </div>
   </section>
