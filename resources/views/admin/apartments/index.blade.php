4f09e739071ec95c3657536649f68406b8fb0286
casalaspro <casalaspro.alessandro@gmail.com>
2024-07-05 18:05:23 +0200

    FIX - minor logic and layout fixes

--- resources/views/admin/apartments/index.blade.php
+++ resources/views/admin/apartments/index.blade.php
@@ -20,37 +20,39 @@
         @else
             <h2 class="text-start my-4">Appartamenti creati</h2>
             <a class="btn btn-primary mb-3" href="{{route('admin.apartments.create')}}"> Crea un nuovo appartamento</a>
-            <p><a class="btn btn-danger" href="{{ route('admin.apartments.index', ['trash' => 1]) }}">Cestino (n)</a></p>
+            <p><a class="btn btn-danger" href="{{ route('admin.apartments.index', ['trash' => 1]) }}">Cestino</a></p>
         @endif
 
         <div class="row gx-3 gy-3 text-center my-3">
             @foreach ($apartments as $apartment)
             <div class="col-12 col-lg-6 position-relative ">
-                <div class="card my-card-apartment h-100">
+                <div class="card my-card-apartment h-100 py-3">
                     <div class="card-body">
                         <div class="img_banner">
                             <img src="{{asset('storage/'.$apartment->img_apartment)}}" alt="">
                             <div class="banner">
-                                <h3>
+                                <h3 class="my-3">
                                     {{$apartment->title_apartment}}
 
                                 </h3>
+                                @if(!$apartment->trashed())
                                 <div class="mt-3">
                                     <a class="btn btn-primary" href="{{route('admin.apartments.show',$apartment)}}">Info Appartamento</a>
                                 </div>
+                                @endif
                                 <div>
                                     {{-- it allows the creation of a button if the apartment is been soft deleted --}}
                                     @if ($apartment->user_id === Auth::id())
                                     @if($apartment->trashed())
                                     {{-- it sent the apartment id to the Apartment Controller through the route --}}
-                                    <form class="delete-form destroy-form" action="{{ route ('admin.apartments.forceDestroy',$apartment->id) }}"  method="POST">
+                                    {{-- <form class="delete-form destroy-form" action="{{ route ('admin.apartments.forceDestroy',$apartment->id) }}"  method="POST">
                                     
                                         @csrf
                                         @method('DELETE')
                                     
                                         <button class="btn btn-danger my-3">Elimina definitivamente</button>
                                     
-                                    </form>
+                                    </form> --}}
 
                                     <form class="delete-form" action="{{ route ('admin.apartments.restore',$apartment->id) }}"  method="POST">
                                     
