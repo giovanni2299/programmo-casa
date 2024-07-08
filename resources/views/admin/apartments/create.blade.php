5a2ab0c7fd2f52c2e2b72cb0ba56e0113e4806fe
casalaspro <casalaspro.alessandro@gmail.com>
2024-07-08 15:09:53 +0200

    ADD - js validation on create.blade.php

--- resources/views/admin/apartments/create.blade.php
+++ resources/views/admin/apartments/create.blade.php
@@ -16,7 +16,7 @@
                         <div>
                             <label for="title_apartment">Titolo del Appartamento:</label>
                         </div>
-                        <input type="text" name="title_apartment" class="form-control" id="title_apartment" placeholder="Inserisci il titolo del Appartamento" value="{{ old('title_apartment') }}" required>
+                        <input type="text" name="title_apartment" class="form-control my-error_check" id="title_apartment" placeholder="Inserisci il titolo del Appartamento" value="{{ old('title_apartment') }}" required>
                     </div>
 
                         @foreach ($errors->get('title_apartment') as $message)
@@ -27,7 +27,7 @@
                         <div>
                             <label for="rooms">N° di Stanze:</label>
                         </div>
-                        <input type="number" name="rooms" class="form-control" id="rooms" placeholder="Inserisci le Stanze Presenti" value="{{ old('rooms') }}">
+                        <input type="number" name="rooms" class="form-control my-error_check" id="rooms" placeholder="Inserisci le Stanze Presenti" value="{{ old('rooms') }}">
 
                         @foreach ($errors->get('rooms') as $message)
                             <li class="my-3 alert alert-danger py-1">{{ $message }}</li>
@@ -38,7 +38,7 @@
                         <div>
                             <label for="beds">N° di Letti:</label>
                         </div>
-                        <input type="number" name="beds" class="form-control" id="beds" placeholder="Inserisci i Letti Presenti" value="{{ old('beds') }}">
+                        <input type="number" name="beds" class="form-control my-error_check" id="beds" placeholder="Inserisci i Letti Presenti" value="{{ old('beds') }}">
 
                         @foreach ($errors->get('beds') as $message)
                             <li class="my-3 alert alert-danger py-1">{{ $message }}</li>
@@ -49,7 +49,7 @@
                         <div>
                             <label for="bathrooms">N° di Bagni:</label>
                         </div>
-                        <input type="number" name="bathrooms" class="form-control" id="bathrooms" placeholder="Inserisci i Bagni Presenti" value="{{ old('bathrooms') }}">
+                        <input type="number" name="bathrooms" class="form-control my-error_check" id="bathrooms" placeholder="Inserisci i Bagni Presenti" value="{{ old('bathrooms') }}">
 
                         @foreach ($errors->get('bathrooms') as $message)
                             <li class="my-3 alert alert-danger py-1">{{ $message }}</li>
@@ -60,7 +60,7 @@
                         <div>
                             <label for="sqr_meters">Metri Quadrati:</label>
                         </div>
-                        <input type="number" name="sqr_meters" class="form-control" id="sqr_meters" placeholder="Inserisci i Metri Quadrati" value="{{ old('sqr_meters') }}">
+                        <input type="number" name="sqr_meters" class="form-control my-error_check" id="sqr_meters" placeholder="Inserisci i Metri Quadrati" value="{{ old('sqr_meters') }}">
 
                         @foreach ($errors->get('sqr_meters') as $message)
                             <li class="my-3 alert alert-danger py-1">{{ $message }}</li>
@@ -71,7 +71,7 @@
                         <div>
                             <label for="img_apartment" class="form-label">Foto dell'Appartamento</label>
                         </div>
-                        <input class="form-control" type="file" id="img_apartment" name="img_apartment">
+                        <input class="form-control my-error_check" type="file" id="img_apartment" name="img_apartment" accept="image/*">
 
                         @foreach ($errors->get('img_apartment') as $message)
                             <li class="my-3 alert alert-danger py-1">{{ $message }}</li>
@@ -83,7 +83,7 @@
                         <div>
                             <label for="description">Descrizione dell'Appartamento:</label>
                         </div>
-                        <input type="text" name="description" class="form-control" id="description" placeholder="Inserisci Info Generali" value="{{ old('description') }}">
+                        <input type="text" name="description" class="form-control my-error_check" id="description" placeholder="Inserisci Info Generali" value="{{ old('description') }}">
 
                         @foreach ($errors->get('description') as $message)
                             <li class="my-3 alert alert-danger py-1">{{ $message }}</li>
@@ -92,7 +92,7 @@
 
                     <div class="mb-3">
                         <label for="complete_address" class="form-label">Indirizzo Completo:</label>
-                        <input type="text" class="form-control my-input-address" id="complete_address" name="complete_address" value="{{ old('complete_address') }}" required placeholder="Inserisci la Via e scegli tra quelle suggerite">
+                        <input type="text" class="form-control my-input-address my-error_check" id="complete_address" name="complete_address" value="{{ old('complete_address') }}" required placeholder="Inserisci la Via e scegli tra quelle suggerite">
                         <div class="invalid-feedback">
                             Per favore inserisci una città valida.
                         </div>
@@ -116,7 +116,7 @@
                             <div class="row">
                                 @foreach ($services as $service)
                                     <div class="form-check col-lg-3 col-md-6 col-sm-12 mb-3">
-                                        <input @checked(in_array($service->id, old('services',  [])))        type="checkbox" name='services[]' id='service-{{    $service->id }}'   value='{{ $service->id }}'>
+                                        <input class="" @checked(in_array($service->id, old('services',  [])))        type="checkbox" name='services[]' id='service-{{    $service->id }}'   value='{{ $service->id }}'>
                                         <label for="weapon-{{ $service->id }}">
                                             {{ $service->name }}
                                         </label>
