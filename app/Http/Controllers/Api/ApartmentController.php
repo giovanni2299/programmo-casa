7667ae6899c0259c56ed3243b65b775e87bf3dde
RoccoCerro <rcerro02@gmail.com>
2024-07-08 18:24:01 +0200

    Add: incomplete coll for tom tom for search api

--- app/Http/Controllers/Api/ApartmentController.php
+++ app/Http/Controllers/Api/ApartmentController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Apartment;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class ApartmentController extends Controller
 {
@@ -18,9 +19,12 @@ class ApartmentController extends Controller
     }
 
     public function search(Request $request){
-        
-
         $zone = $request->input('zone');
+
+        // $response = Http::get('https://api.tomtom.com/search/2/geocode/'.$zone.'.json?storeResult=false&countrySet=ITA&lat=41.9027835&lon=12.4963655&view=Unified&key=SmzJJ1e9vacLwiqfqgxPWAvQ7Ey33PfG')->json();
+        
+        // $results = $response['results'][0]['position'];
+        // @dd($results);
         $apartments = Apartment::where('complete_address', 'like', '%'.$zone.'%')->get();
         
         return response()->json($apartments);
