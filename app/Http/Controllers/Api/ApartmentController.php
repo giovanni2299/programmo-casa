0f0f75337afd1ee6e7b1786fc666bba782f7bb0d
RoccoCerro <rcerro02@gmail.com>
2024-07-09 16:52:13 +0200

    Add: search in the controller apartment with coordinates

--- app/Http/Controllers/Api/ApartmentController.php
+++ app/Http/Controllers/Api/ApartmentController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Apartment;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Http;
 
 class ApartmentController extends Controller
@@ -19,19 +20,18 @@ class ApartmentController extends Controller
     }
 
     public function search(Request $request){
-        $zone = $request->input('zone');
-        // $min_lat = $request->input('min_lat');
-        // $max_lat = $request->input('max_lat');
-        // $min_lon = $request->input('min_lon');
-        // $max_lon = $request->input('max_lon');
+        // $zone = $request->input('zone');
+        $min_lat = $request->input('min_lat');
+        $max_lat = $request->input('max_lat');
+        $min_lon = $request->input('min_lon');
+        $max_lon = $request->input('max_lon');
 
-        // $response = Http::get('https://api.tomtom.com/search/2/geocode/'.$zone.'.json?storeResult=false&countrySet=ITA&lat=41.9027835&lon=12.4963655&view=Unified&key=SmzJJ1e9vacLwiqfqgxPWAvQ7Ey33PfG')->json();
-        // // '.$zone.'
-        // $lat = $response['results'][0]['position']['lat'];
-        // $lon = $response['results'][0]['position']['lat'];
-
-        $apartments = Apartment::where('complete_address', 'like', '%'.$zone.'%')->get();
-        // $apartments = Apartment::whereBetween('latitude', [$max_lat, $min_lat] )->get();
+        $apartments = DB::table('apartments')
+                        ->where( 'latitude', '>', $min_lat) 
+                        ->where('latitude', '<', $max_lat)
+                        ->where('longitude', '<', $max_lon )
+                        ->where('longitude', '>', $min_lon )
+                        ->get();
         
         return response()->json($apartments);
     }
