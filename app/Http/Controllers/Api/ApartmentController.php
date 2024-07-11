7549cbdbf28388054911ace846c869e451955b5a
RoccoCerro <rcerro02@gmail.com>
2024-07-11 17:12:17 +0200

    Refactor: apartment controller

--- app/Http/Controllers/Api/ApartmentController.php
+++ app/Http/Controllers/Api/ApartmentController.php
@@ -50,39 +50,14 @@ class ApartmentController extends Controller
         $jsonArray = json_decode($request,true);
         $content = json_decode($request->getContent(), true);
 
-
         $min_lat = $content['min_lat'];
-        // @dd($min_lat);
         $max_lat = $content['max_lat'];
         $min_lon = $content['min_lon'];
         $max_lon = $content['max_lon'];
+        
         $serviceArray = $content['activeFilters'];
 
-        // $apartments = DB::table('apartments')->select('apartments.*')
-        // ->join('apartment_service', 'apartment_id', '=', 'apartments.id')
-        // ->join('apartment_service', 'service_id', '=', 'services.id')
-        // ->where( 'latitude', '>', $min_lat ) 
-        // ->where('latitude', '<', $max_lat )
-        // ->where('longitude', '<', $max_lon )
-        // ->where('longitude', '>', $min_lon )
-        // ->whereIn('service.name', 'wi-fi')->groupBy('apartment_id')->get();
         $serviceIds = $request->input('service_ids');
-
-        // Recupera gli appartamenti che possiedono i servizi specificati
-        // $apartments = Apartment::whereHas('services', function($query) use ($serviceArray) {
-        //     $query->whereIn('id', $serviceArray);
-        // })->count($serviceIds)->get();
-
-        // $apartments = DB::table('apartments')
-        // ->join('services', 'apartment_service.service_id', '=', 'service.id')
-        // ->join('apartment', 'apartment_service.apartment_id', '=', 'apartment.id')
-        // ->select('apartment.*', 'services.*')->get();
-
-        // $apartments = Apartment::with('services')
-        // ->get();
-        //--------------------------------------------
-
-        $servizio = ["pasta scotta", "wi-fi", "animali"];
 
         $apartments = Apartment::where( 'latitude', '>', $min_lat ) 
             ->where('latitude', '<', $max_lat )
@@ -92,15 +67,6 @@ class ApartmentController extends Controller
             $query->whereIn('id', $serviceArray);
         }, '=', count($serviceArray))->get();
 
-        
-
-        //--------------------------------------------
-
-
-        // $result = Pool::join('Contribution', 'Pool.id', '=', 'Contribution.pool_id')
-        // ->join('Contributer', 'Contribution.contributer_id', '=', 'Contributer.id')->get();
-
-        // dd($apartments);
 
         return response()->json([
             'success' => true,
